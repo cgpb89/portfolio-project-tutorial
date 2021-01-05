88b20a879d0ecfbe1f557732021c06db5e02f33d
Carlos Pereira <carlosp@thehangar.cr>
2021-01-04 19:58:23 -0600

    NavBar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
-<h2>Test</h2>  );
+    <Navbar/>
+ );
 }
 
 export default App;
